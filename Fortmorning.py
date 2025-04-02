563b099e045e5fcb356af98c9483d2d4b7de6d9c
Vibhav Dhathrika <Vibhavdhathrika@gmail.com>
2025-04-02 21:13:46 +0530

    Update Fortmorning.py

--- Fortmorning.py
+++ Fortmorning.py
@@ -3,17 +3,14 @@ import time
 running = True
 
 while running:
+    player_pos_y = 0
+    player_pos_x = 0
+    
     peeler_val = random.randint(0, 426)
 
     venomelon_val = random.randint(0, 100000000)
 
-    choice = input("Want to enter building?(y/n)")
+    movement = input("Where do you want to go??(W/A/S/D)")
 
-    
-
-    if monster_val % 6 == 0 and choice == "y":
-        print("aye!! jiggle booty get over here")
-    eilf venomelon_val % 109 == 0 and choice =="y":
-        print("How lucky you are to be touched by veno the melon!")
-    else:
-        print("i'll get u bagude wait da")
+    if movement == "W":
+        
